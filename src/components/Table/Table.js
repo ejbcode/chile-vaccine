bd49128b70b6b3821a75216cd5ebf39d74ed7029
Ernesto B <ejbcode@gmail.com>
2021-04-22 14:47:32 -0400

    Styles in table remove unnecessary files change favicon reconvert _app.js to tsx

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,7 +4,6 @@ const Table = ({allStats}) => {
   return (
     <section>
       <h2>Distribución Regional en el país</h2>
-      {/* <p>Lorem ipsum dolor sit amet.</p> */}
       <InsideTable DATA={allStats} />
     </section>
   );
